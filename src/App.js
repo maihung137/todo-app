9620ed1a34cac70f544bb97b0705e05973ff6185
mai-hung <maihung1372002@gmail.com>
2022-10-24 21:26:57 +0700

    create todo-app with some firts UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+let items = [
+    {
+        id: 1,
+        job: 'something',
+        checked: true,
+    },
+    {
+        id2: 2,
+        job: 'quetnha',
+    },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="row">
+                    <h2 className="">Thing todo</h2>
+                    <ul className="list">
+                        {items.map((item) => (
+                            <li key={item.id} className="">
+                                <input type={'checkbox'} checked={item.checked} />
+                                {item.job}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
